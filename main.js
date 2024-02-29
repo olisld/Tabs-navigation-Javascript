0c075501613e256867648d7033b761ef93d00274
olisld <olivier.soulard4@gmail.com>
2024-02-29 16:57:20 +0100

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 /**
  * @param {event} evt
  * @param {string} cityName
- */
+ */ 
 function openCity(evt, cityName) {
   console.log(evt, cityName);
-  // code
+  tabcontents = document.getElementsByClassName('tabcontent');
+  for (let i =0;i<tabcontents.length;i++){
+    tabcontents[i].style.display = 'none'
+  }
+  
+  document.getElementById(cityName).style.display = 'block';
 }
+
+  
+  // code
